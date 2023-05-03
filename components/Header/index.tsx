75b9a2da87e05e4ae971d0391066a3eff1a488e3
izat <izat@izat>
2023-05-03 20:25:25 +0300

    First

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -22,8 +22,8 @@ const Header = () => {
                 <div className='flex items-center'>
                     <WalletDropdown />
                     <button
-                    onClick={()=> {}}
-                    className='flex items-center ml-5'>
+                        onClick={() => { }}
+                        className='flex items-center ml-5'>
                         <Image src={locationIcon} width={20} alt="" />
                         <p className='mx-2 text-gray-500'>{location.city}</p>
                     </button>
@@ -38,30 +38,41 @@ const Header = () => {
                     <h1 className='mx-2 text-4xl font-bold'></h1>
                 </div>
                 <div className='flex items-center'>
-                    <div className='mr-10'>
+                    <div className='mr-10 lg:block hidden '>
                         <input
                             value={searchInputValue}
                             onChange={(e) => setSearchInputValue(e.target.value)}
-                            className='w-[600px] rounded-md outline-none text-xl border-2 border-blue-600 border-solid text-black px-7 py-2'
+                            className=' w-[360px] xl:w-[600px] rounded-l-md outline-none text-xl border-2 border-blue-600 border-solid text-black px-7 py-2'
                             type="search" placeholder='Я ищу...' name="header-search-input" id="header-search-input" />
+                        <button className='bg-blue-600 py-[10px] px-5 text-xl text-white font-bold rounded-r-md'>Search</button>
                     </div>
                     <div>
                         <nav className='flex'>
                             <Link href="/login" className='mx-3 flex flex-col justify-center items-center'>
-                                <Person size={20}/>
+                                <Person size={20} />
                                 <p className='text-lg'>Войти</p>
                             </Link>
                             <Link href="/favorites" className='mx-3 flex flex-col justify-center items-center' >
-                                <Heart size={20}/>
+                                <Heart size={20} />
                                 <p className='text-lg'>Избранное</p>
                             </Link>
-                            <Link  href="basket" className='mx-3 flex flex-col justify-center items-center'>
-                                <Basket size={20}/>
+                            <Link href="basket" className='mx-3 flex flex-col justify-center items-center'>
+                                <Basket size={20} />
                                 <p className='text-lg'>Корзина</p>
                             </Link>
                         </nav>
                     </div>
+
                 </div>
+
+            </div>
+            <div className='flex items-center justify-center'>
+                <input
+                    value={searchInputValue}
+                    onChange={(e) => setSearchInputValue(e.target.value)}
+                    className='w-full rounded-l-md outline-none text-xl border-2 border-blue-600 border-solid text-black px-7 py-2'
+                    type="search" placeholder='Я ищу...' name="header-search-input" id="header-search-input" />
+                <button className='bg-blue-600 py-[10px] px-5 text-xl text-white font-bold rounded-r-md'>Search</button>
             </div>
         </header>
     )
